0145b4fe432caeb98bd808594f579edf788020d5
arikoda <arikoda72126@gmail.com>
2020-09-24 11:28:26 +0900

    編集・削除・検索・詳細機能

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,12 +1,24 @@
 class FoodsController < ApplicationController
-
+  before_action :set_food, only: [:show, :edit, :update, :destroy]
+  #before_action :allergy_string, only: [:create, :edit]
+  before_action :set_date, only: [:index, :show, :search]
   def index
+    @foods = Food.all.order("expriation_date ASC")
+
   end
 
-  def food_index
+  def edit
   end
 
   def show
+  end
+
+  def destroy
+    if @food.destroy
+      redirect_to root_path
+    else
+      render :show
+    end
   end
 
   def new
@@ -14,6 +26,40 @@ class FoodsController < ApplicationController
   end
 
   def create
+    @food = Food.create(food_params)
+    #binding.pry
+    if @food.save
+      redirect_to root_path
+    else
+      render :new , alert: @food.errors.full_messages
+    end
+    
   end
 
+  def update
+    @food.update(food_params)
+  end
+
+  def search
+    @foods = Food.search(params[:keyword])
+
+  end
+
+  private
+
+  def food_params
+    params.require(:food).permit(:name, :image, :amount, :expriation_date, :count, :category_id, allergy_id:[])
+  end
+
+  def set_food
+    @food = Food.find(params[:id])
+  end
+
+  def set_date
+    today = Time.new
+    @date = today.since(3.days)
+  end
+ # def allergy_string
+  #  params[:food][:allergy_id].join(",")
+ # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'foods#index'
-  resources :foods, only: [:index, :edit,:new, :create, :show, :destroy]
-  get 'foods', to: 'foods#food_index'
+  root to: 'tops#index'
+  resources :foods do
+    collection do
+      get 'search'
+    end
+  end
 end
