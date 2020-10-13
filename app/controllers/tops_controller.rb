bea53e1c578de0a1a4c57e83b7c7001a54c28bba
arikoda <arikoda72126@gmail.com>
2020-10-13 11:20:36 +0900

    ビューの編集

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,4 +1,5 @@
 class TopsController < ApplicationController
   def index
+    @today = Date.today.strftime('%Y年%m月%d日')
   end
 end
---
+++ app/javascript/packs/index.js
@@ -0,0 +1,12 @@
+//window.onload = function () {
+  //var today = new Date();
+
+   // const days = document.getElementById('day1');
+   // const caution = (today - days <= 3 );
+   // while (caution != " " ){ 
+     //   const day = document.getElementById('day1')
+       // day.style.color ='#FF0000';
+       // console.log
+    //  };
+
+// };
